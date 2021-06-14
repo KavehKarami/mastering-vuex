d0c807cbef71875ad63e61bf7bb411f92609dc01
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-06-14 16:57:21 +0430

    use state and getters and explain diffrences of mutations and actions

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,59 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: "asad213",
+      name: "Kevin Carlsen",
+    },
+    categories: [
+      "sustainability",
+      "nature",
+      "animal welfare",
+      "housing",
+      "education",
+      "food",
+      "community",
+    ],
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+      { id: 3, text: "...", done: true },
+      { id: 4, text: "...", done: false },
+    ],
+  },
+  /**
+   * REVIEW:
+   * "mutations" looks like "reducers" in redux, "mutations" are synchronous
+   * calling "mutations" with "commit"
+   *
+   * "actions" looks like "mutations" but "actions" are asynchronous and also in "actions" you can access to "states, getters, commit"
+   * calling "actions" with "dispatch"
+   *
+   * "commit" and "dispatch" in vuex looks like "dispatch" in redux
+   *
+   * both of "mutations" and "actions" can get "payloads"
+   *
+   * it's better to use "actions" and call "mutation" inside "actions"
+   *
+   */
+
   mutations: {},
   actions: {},
   modules: {},
+
+  // REVIEW: getters looks like a `selectors` in redux
+  getters: {
+    categoryLength: (state) => state.categories.length,
+
+    // getters into getters:
+    // NOTE: only for showing the purpose, it's not a best way
+    doneTodos: (state) => state.todos.filter((todo) => todo.done),
+
+    activeTodosCount: (state, getters) =>
+      state.todos.length - getters.doneTodos.length,
+
+    // dynamic getters
+    findTodo: (state) => (id) => state.todos.find((todo) => todo.id === id),
+  },
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,48 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>this event was created by {{ user.id }}</p>
+
+    <ul>
+      <li v-for="category in categories" :key="category">
+        {{ category }}
+      </li>
+    </ul>
+
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ findTodo(3) }}</p>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+export default {
+  computed: {
+    // one way:
+    /* userName() {
+      return this.$store.state.user.name;
+    }, */
+    // another way:
+    /* ...mapState({
+      userID: (state) => state.user.id,
+      categories: "categories",
+    }), */
+
+    // best way states:
+    ...mapState(["user", "categories"]),
+    // best way for getters:
+    ...mapGetters(["findTodo"]),
+    // ------------------------------------------------------
+    catLength() {
+      // one way:
+      /* return this.$store.state.categories.length; */
+
+      // another way:
+      return this.$store.getters.categoryLength;
+    },
+    // getTodo() {
+    //   return this.$store.getters.findTodo;
+    // },
+  },
+};
+</script>
